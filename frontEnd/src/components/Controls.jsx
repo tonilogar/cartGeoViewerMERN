329d4abb807703374ce2394d409095677050ad24
tonilogar <tologar@gmail.com>
2021-09-08 23:17:35 +0200

    start new frontend

--- frontEnd/src/components/Controls.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/components/Controls.css'
-
-const Controls = () => (
-  <div className="controls" > Controls </div>
-)
-
-export default Controls;
---
+++ frontEndOld/src/components/Controls.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/components/Controls.css'
+ 
+const Controls = () => (
+  <div className="controls" > Controls </div>
+)
+
+export default Controls;
