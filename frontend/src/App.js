cf25f0dab6f2a502e210c9b814270f72164ee85c
pcoticgc <tologar@gmail.com>
2021-09-16 13:59:00 +0200

    start classes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,9 @@ import User from './components/User';
 import Perspective from './components/Perspective'
 import Info from './components/Info'
 import Layers from './components/Layers'
-/* import CatchCoordinates from './components/CatchCoordinates' */
-import Coordinates from './components/Coordinates'
 import SignIn from './components/SignIn'
 import ForgotPass from './components/ForgotPass'
 import SignUp from './components/SignUp'
-import Fondo from './components/Fondo'
 
 
 
@@ -36,8 +33,6 @@ const App = () => (
     <Route exact path="/" component={Perspective}/>
     <Route exact path="/" component={Info}/>
     <Route exact path="/" component={Layers}/>
-    {/* <Route exact path="/" component={CatchCoordinates}/> */}
-    <Route exact path="/" component={Coordinates}/>
     <Route exact path="/signin" component={SignIn}/>
     <Route exact path="/forgotpass" component={ForgotPass}/>
     <Route exact path="/signup" component={SignUp}/> 
