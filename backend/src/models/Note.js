8e7fc7ecc22fb83b9f5657b1b48b3e0b65918a5b
pcoticgc <tologar@gmail.com>
2021-09-20 13:29:23 +0200

    users created with frontEnd

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -3,9 +3,15 @@ const { Schema, model } = require('mongoose');
 const noteSchema = new Schema(
     {
         title: String,
-        content: { type: String, required: true},
-        author: { type: String },
-        date: Date
+        content: { 
+            type: String, 
+            required: true
+        },
+        author: String,
+        date: {
+            type: Date,
+        default: Date.now
+        }
     }, {
         timestamps: true
     });
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -3,41 +3,54 @@ import '../css/SignUp.css'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 export default class SignUp extends Component {
+    
     state = {
-       
-        users: []
-    }
-
-    async componentDidMount() {
-        this.getUsers();
-    }
-
-    getUsers = async () => {
-        const res = await axios.get('http://localhost:4000/api/users');
-        this.setState({
-            users: res.data
-        });
+        users: [],
+        username:''
     }
     
+    //Pedimos los datos al servidor de los usernames.
+    //Cuando entramos en la dirección http://localhost:4000/api/users 
+    //nos muestra con un log los usuarios existentes
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users')
+        this.setState({users: res.data})
+        console.log(this.state.users)
+    }
+
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+
+        console.log(e.target.value)
+    }
+    onSubmit = async e => {
+        e.preventDefault();
+       const res = await axios.post('http://localhost:4000/api/users', {
+           username: this.state.username
+        })
+       console.log(res )
+    }
     render() {
         return (
-            <form className="formSignUp" onSubmit={this.handleSubmit}>     
+            <form className="formSignUp" onSubmit={this.onSubmit}>     
                 <h1> Create your acount </h1>
                 <label className="labelUserName" htmlFor="userName">  User name   </label>
-                <input onChange={this.handleChange} className="inputUserName" type="text" id="userName"/>
-                <label className="labelPassword" htmlFor="password">  Password  </label>
-                <input onChange={this.handleChange} className="inputPassword" type="password" placeholder="********" id="password"/>
+                <input onChange={this.onChangeUsername} className="inputUserName" type="text" id="userName"/>
+               {/*  <label className="labelPassword" htmlFor="password">  Password  </label>
+                <input onChange={this.onChangeData} className="inputPassword" type="password" placeholder="********" id="password"/>
                 <label className="labelConfirmPassword" htmlFor="confirmPassword">  Confirm password  </label>
-                <input onChange={this.handleChange} className="inputConfirmPassword" type="password" placeholder="********" id="confirmPassword"/>
+                <input onChange={this.onChangeData} className="inputConfirmPassword" type="password" placeholder="********" id="confirmPassword"/>
                 <label className="labelEmail" htmlFor="email">  Email   </label>
-                <input onChange={this.handleChange} className="inputEmail" type="email" id="email"/>
-                <input onClick={this.handleSubmit}  className="buttonCreateUser" type="submit" value="Create user"/>
-                <button onClick={this.handleClick} className="buttonHome"  type="button"><Link to="/">Home</Link></button>
+                <input onChange={this.onChangeData} className="inputEmail" type="email" id="email"/>*/}
+                <input onChange={this.onSubmit} className="buttonCreateUser" type="submit" value="Create user"/>
+                {/* <button className="buttonHome"  type="button"><Link to="/">Home</Link></button>
                 <footer className="footer">
                     <Link to='/'>Terms</Link>
                     <Link to='/'>Privacy</Link>
                     <Link to='/'>helpt</Link>
-                </footer>
+                </footer>  */}
             </form>
         )
     }
